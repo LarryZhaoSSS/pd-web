31fadbfcdfcabe10c27ccc0ce4c2ad72c38bb07e
LarryKameZhao <larryzhao1994@outlook.com>
2019-07-10 07:48:35 +0800

    group-order component

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { ProductRoutingModule } from './product-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { ProductContainerComponent } from './components/product-container';
+import { GroupItemComponent } from './components';
+import { GroupShortListComponent } from './components/group-short-list/group-short-list.component';
 
 @NgModule({
   declarations: [
-    ProductContainerComponent
+    ProductContainerComponent,
+    GroupItemComponent,
+    GroupShortListComponent
   ],
   imports: [SharedModule, ProductRoutingModule]
 })
